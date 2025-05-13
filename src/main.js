8a15ef62bb9a0dc94f2c78d933be6e58bef0dcc1
mauro <mauro_leao@hotmail.com>
2025-05-13 10:14:17 -0300

    feat: add header with navigation and scroll behavior, update styles for header and buttons

--- src/main.js
+++ src/main.js
@@ -1,27 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll('[data-tab-button]');
   const questions = document.querySelectorAll("[data-faq-question]");
-  
 
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", function (botao) {
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeTodasAbas();
-            aba.classList.add('shows__list--is-active');
-            removeBotaoAtivo();
-            botao.target.classList.add("shows__tabs__button--is-active");
+  const heroSection = this.documentElement.querySelector(".hero");
+  const alturaHerro = heroSection.clientHeight;
 
-               
-        });
+  window.addEventListener("scroll", function () {
+    const posicaoAtual = window.scrollY;
 
-    }    
+    if (posicaoAtual < alturaHerro) {
+      ocultaElementoDoHeader();
+
+    } else {
+      
+      exibeElementoDoHeader();
+
+    }
     
+  });
+
+  // Seção de atrações, programação das abas
+  for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function (botao) {
+          const abaAlvo = botao.target.dataset.tabButton;
+          const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+          escondeTodasAbas();
+          aba.classList.add('shows__list--is-active');
+          removeBotaoAtivo();
+          botao.target.classList.add("shows__tabs__button--is-active");
+
+      });
+  }    
     
+  // Seção Faq, acordeão
   for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", abreOuFechaResposta);
   }
+  
 });
+
+
+function ocultaElementoDoHeader() {
+  const header = document.querySelector(".header");
+  header.classList.add("header--is-hidden");
+}
+
+function exibeElementoDoHeader() {
+  const header = document.querySelector(".header");
+  header.classList.remove("header--is-hidden");
+}
+
+
 
 function abreOuFechaResposta(elemento) {
   const classe = "faq__questions__item--is-open";
